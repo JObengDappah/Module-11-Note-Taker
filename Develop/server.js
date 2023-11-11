8e718042424ac247d84ad4f1606d9940c661505d
JObengDappah <134343671+JObengDappah@users.noreply.github.com>
2023-11-11 17:03:40 -0500

    packages redownloaded

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const html_Routes = require('./routes/html-routes');
-const api_Routes = require('./routes/api-routes');
+const html_routes = require('./routes/api-routes');
+const api_routes = require('./routes/html-routes');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(express.static('public'));
 app.use(api_Routes);
 app.use(html_Routes);
